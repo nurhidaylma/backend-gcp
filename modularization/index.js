3aeea7f2775d3e777ba8dfc4aafc75f8ea1fe8f8
nurhidaylma <ilmanurhidayati752@gmail.com>
2022-06-07 11:36:43 +0700

    Export and import module in Node.js

---
+++ modularization/index.js
@@ -0,0 +1,23 @@
+// Import module Tiger and Wolf
+const Tiger = require('./Tiger'); 
+const Wolf = require('./Wolf');
+
+const fighting = (tiger, wolf) => {
+  if(tiger.strength > wolf.strength) {
+    tiger.growl();
+    return;
+  }
+
+  if(wolf.strength > tiger.strength) {
+    wolf.howl();
+    return;
+  }
+
+  console.log('Tiger and Wolf have the same strength');
+}
+
+// Create object from module Tiger and Wolf
+const tiger = new Tiger(); 
+const wolf = new Wolf();
+
+fighting(tiger, wolf);
