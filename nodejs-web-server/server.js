8468bdd8335b5441bb6da9a2bd127622cb0a81bf
nurhidaylma <ilmanurhidayati752@gmail.com>
2022-06-12 14:06:21 +0700

    Add status code as response

--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -3,19 +3,20 @@ const http = require('http')
 const requestListener = (request, response) => {
   response.setHeader('Content-Type', 'text/html');
 
-  response.statusCode = 200;
-
   const { url, method } = request;
 
   if (url === '/') {
     if(method === 'GET') {
+      response.statusCode = 200;
       response.end('<h1>Ini adalah homepage</h1>');
+    } else {
+      response.statusCode = 400;
+      response.end(`<h1>Halaman tidak dapat diakses dengan method ${method} </h1>`);
     }
-
-    response.end(`<h1>Halaman tidak dapat diakses dengan method ${method} </h1>`);
   } 
   else if (url === '/about') {
     if (method === 'GET') {
+      response.statusCode = 200;
       response.end('Halo! Ini adalah halaman about');
     } 
     else if(method === 'POST') {
@@ -30,12 +31,15 @@ const requestListener = (request, response) => {
       request.on('end', () => {
         body = Buffer.concat(body).toString();
         const { name } = JSON.parse(body); // to cast the json string to js object 
+        response.statusCode = 200;
         response.end(`<h1>Hai, ${name}! Ini adalah halaman about</h1>`);
       });
     } else {
+      response.statusCode = 400;
       response.end(`Halaman tidak bisa diakses menggunakan method ${method}`);
     }
   } else {
+    response.statusCode = 404;
     response.end('<h1>Halaman tidak ditemukan</h1>');
   }
 };
